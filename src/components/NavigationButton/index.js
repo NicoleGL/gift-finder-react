24424d012636583c43c5fb1cd87133dbd8526fc6
Nicole Garcia Langa <nicole.garcia-langa@klarna.com>
2021-08-20 10:35:32 +0200

    Fixed styling issues with hover

--- src/components/NavigationButton/index.js
+++ src/components/NavigationButton/index.js
@@ -4,11 +4,13 @@ import styles from "./index.module.css";
 
 function NavigationButton(props) {
   const { text, to, color, width, icon, click } = props;
+  const hoverClass = color === "var(--purple)" ? "purple-hover" : "black-hover";
+
   return (
     <Link
       onClick={click ? () => click() : () => {}}
       to={to}
-      className={styles["Navigation-button"]}
+      className={`${styles["Navigation-button"]} ${styles[hoverClass]}`}
       style={{ backgroundColor: color, width: width }}
     >
       {<i className={`${styles["fa-mar"]} ${icon}`}></i>}
